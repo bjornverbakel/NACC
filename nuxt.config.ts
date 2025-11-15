5e8267f539c753e3c1d63d589c3a26b62512688f
Bjorn Verbakel <bjorn.verbakel@student.fontys.nl>
2025-11-15 22:18:18 +0100

    more style config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,16 +9,6 @@ export default defineNuxtConfig({
 
   css: ['~/assets/styles/main.scss'],
 
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: '@import "@/assets/styles/_variables.scss"; @import "@/assets/styles/_mixins.scss";',
-        },
-      },
-    },
-  },
-
   vuetify: {
     moduleOptions: {
       /* module specific options */
