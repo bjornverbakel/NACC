5516835a5bff5f7a9fb1bf280b032cac1864958b
Bjorn Verbakel <bjorn.verbakel@student.fontys.nl>
2025-11-22 21:23:20 +0100

    rm old component

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -2,7 +2,6 @@
   <v-main>
     <v-container fluid class="fill-height d-flex align-center justify-center">
       <v-card max-width="500" width="100%" class="pa-4">
-        <AppBackToLogin />
         <slot />
         <v-card-text class="text-caption text-center mt-4">
           This site is authenticated by supabase.com and the Supabase
