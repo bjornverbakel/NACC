c789c8243681b8e2057c1cb2f45dbecd19d69f88
bjorn <bjornverbak2006@gmail.com>
2025-12-16 11:25:06 +0100

    fix supabase db indexes

--- app/pages/settings.vue
+++ app/pages/settings.vue
@@ -2,16 +2,12 @@
   <div class="section-spacing">
     <h1 class="main-header">Settings</h1>
 
-    <!-- Import Game Save -->
     <ImportGameSave />
 
-    <!-- Manage Completion Data -->
     <ManageCompletionData />
 
-    <!-- Account Settings -->
     <AccountSettings v-if="!isAnonymous && user" />
 
-    <!-- Danger Zone -->
     <DangerZone v-if="user" />
   </div>
 </template>
